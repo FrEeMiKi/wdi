c864f8813c04a2ef9e4c507648cec5921de5b719
Mikołaj Majcerek <72302028+FrEeMiKi@users.noreply.github.com>
2023-10-18 15:05:17 +0200

    Update zadanie_9.py

--- zadanie_9.py
+++ zadanie_9.py
@@ -1,16 +1,35 @@
 x = int(0)
-pin = int(input("Ustal swój nr PIN: "))
-def autoryzacja():
-    if int(input("Podaj swój nr PIN: ")) == pin:
+pin = int
+print("Witamy w banku KKO!")
+while True:
+    while pin not in range(1000,9999):
+        try:
+            pin = int(input("Ustal swój czterocyfrowy nr PIN: "))
+        except ValueError:
+            pass
+    try:
+        if int(input("Powtórz swój nr PIN: ")) == pin:
+            break
+        else:
+            pin = 0
+    except ValueError:
         pass
-    else:
-        ValueError
+        
+
     
-
-
+def autoryzacja():
+    n = int(0)
+    for _ in range(3):
+        if int(input("Podaj swój nr PIN: ")) == pin:
+            break
+        elif _ == 2:
+            print("Wpisano nieprawidłowy PIN 3 razy. Konto zostało zablokowane.")
+            raise(SystemExit)
+        
+        
 while True:
     try:
-        print("Jaką operację chcesz wykonać?\na) wpłata\nb) wypłata\nc) saldo konta\nd) wyjście")
+        print("Jaką operację chcesz wykonać?\na) Wpłata\nb) Wypłata\nc) Saldo konta\nd) Wyjście")
         wybor = input("Wybór: ")
         if wybor == "a":
             autoryzacja()
@@ -20,12 +39,14 @@ while True:
             x = x - int(input("Kwota wypłaty: "))
         elif wybor == "c":
             autoryzacja()
-            print("Saldo:", x, "zł")
+            print("\n###############\nSaldo:", x, "zł\n###############\n")
         elif wybor == "d":
             print("Do Widzenia!")
-            False
+            break
     except ValueError:
         pass
+    except SystemExit:
+        break
     
     
     
