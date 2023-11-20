ad7c7ffea4c35bc0e453b98691f111fc5ec1ee14
Mikołaj Majcerek <72302028+FrEeMiKi@users.noreply.github.com>
2023-11-20 21:48:10 +0100

    Update macierz.py

--- macierz.py
+++ macierz.py
@@ -48,4 +48,5 @@ for row in y:
 max_value = max(max(row) for row in y)
 max_row_col = [(i + 1, j + 1) for i, row in enumerate(y) for j, value in enumerate(row) if value == max_value]
 
+print("Maximum value is", max_value")
 print("Maximum value's row and column:", max_row_col)
